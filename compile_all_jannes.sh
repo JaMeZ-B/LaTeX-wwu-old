859413fb5eb5f351640c78032d53c9bec0ade1c1
JaMeZ-B <j.bantje@wwu.de>
2015-02-18 21:40:41 +0100

    layout changes (BCOR, DIV), moved the qr-codes to a separate page and added a "compile all" script and started using it

---
+++ compile_all_jannes.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+declare -a arr=("Ana1_WS12/analysis1.tex" "LA1_WS12/lineare_algebra1.tex" "Ana2_SS13/analysis2.tex" "LA2_SS13/lineare_algebra2.tex" "Ana3_WS13/analysis3.tex" "EinfAlg_WS13/algebra.tex" "DiffMa_SS14/diff_ma.tex" "HoehAlg1_SS14/hoehere_algebra.tex" "AnaTopGeo_SS14/ana_top_geo.tex" "Topo1_WS14/topologie_1.tex" "FunkAna_WS14/funktional_analysis.tex")
+for i in "${arr[@]}"
+do
+	latexmk -xelatex -cd -silent "$i"
+done
+exit 0
+
